20702b21795bd6ddb0235327147c42df3f01d1e8
Cédric Messiant <cedricmessiant@ecreall.com>
2017-05-10 14:41:57 +0200

    Configure store with redux devtools (only in development mode)

--- assembl/static2/webpack.dev.config.js
+++ assembl/static2/webpack.dev.config.js
@@ -82,5 +82,10 @@ module.exports = {
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+          'process.env': {
+            NODE_ENV: JSON.stringify('development')
+          }
+        }),
     ]
 };
