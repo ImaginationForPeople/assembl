c56ca891594505b54cd2a1c8d0d985231bbd27f0
Pascal Huynh <pascal@beyowi.com>
2019-01-21 17:37:47 +0100

    create semantic analysis page (debate level)

--- assembl/static2/js/app/utils/translations.js
+++ assembl/static2/js/app/utils/translations.js
@@ -1313,7 +1313,7 @@ const Translations = {
         tooltip: "Tooltip icon"
       },
       loader: {
-        error: "TO BE DEFINED",
+        error: "There is an error loading the analysis",
         loading: "Loading..."
       },
       wordCountInformation: {
